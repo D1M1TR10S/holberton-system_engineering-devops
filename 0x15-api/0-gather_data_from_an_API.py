67f50318fe44fd48b7d33a829e4b1155e0c3f929
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-01 03:07:28 +0000

    Check for integer passed as argument

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,19 +9,19 @@ import sys
 
 
 if __name__ == "__main__":
-    staff = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
-    employee = staff.json()['name']
-    user_id = staff.json()['id']
-    
-    todo_list = requests.get('https://jsonplaceholder.typicode.com/todos')
-    done = []
-    fin = total = 0
-    for dct in todo_list.json():
-        if dct['userId'] == user_id:
-            if dct['completed'] == True:
-                done.append(dct['title'])
-                fin += 1
-            total += 1
-    print("Employee {} is done with tasks({}/{}):".format(employee, fin, total))
-    for i in done:
-        print("\t " + i)
+    if sys.argv[1].isdigit():
+        staff = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+        employee = staff.json()['name']
+        user_id = staff.json()['id']
+        todo_list = requests.get('https://jsonplaceholder.typicode.com/todos')
+        done = []
+        fin = total = 0
+        for dct in todo_list.json():
+            if dct['userId'] == user_id:
+                if dct['completed'] == True:
+                    done.append(dct['title'])
+                    fin += 1
+                total += 1
+        print("Employee {} is done with tasks({}/{}):".format(employee, fin, total))
+        for i in done:
+            print("\t " + i)
