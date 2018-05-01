d01dfa64b760e9d42639a041ca7aa7a2d50f692b
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-01 03:24:48 +0000

    Cleaned up redundancies and used .get() to refer to dict items

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,23 +5,23 @@ employee's TODO list progress.
 Call the JSON placeholder REST api.
 '''
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    if sys.argv[1].isdigit():
-        staff = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
-        employee = staff.json()['name']
-        user_id = staff.json()['id']
-        todo_list = requests.get('https://jsonplaceholder.typicode.com/todos')
+    if argv[1].isdigit():
+        staff = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+        employee = staff.json().get('name')
+        todo_list = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'
+            .format(argv[1])).json()
         done = []
-        fin = total = 0
-        for dct in todo_list.json():
-            if dct['userId'] == user_id:
-                if dct['completed'] == True:
-                    done.append(dct['title'])
-                    fin += 1
-                total += 1
-        print("Employee {} is done with tasks({}/{}):".format(employee, fin, total))
+        fin = 0
+        for dct in todo_list:
+            if dct.get('completed') is True:
+                done.append(dct['title'])
+                fin += 1
+        print("Employee {} is done with tasks({}/{}):".format(employee, fin, len(todo_list)))
         for i in done:
             print("\t " + i)
