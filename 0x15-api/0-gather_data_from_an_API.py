07529ecff87396d39d500321a7c5fab16adb2734
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-01 02:55:21 +0000

    Find an employee through an api and print the tasks that they've completed

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+Returns information about a given
+employee's TODO list progress.
+Call the JSON placeholder REST api.
+'''
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    staff = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+    employee = staff.json()['name']
+    user_id = staff.json()['id']
+    
+    todo_list = requests.get('https://jsonplaceholder.typicode.com/todos')
+    done = []
+    fin = total = 0
+    for dct in todo_list.json():
+        if dct['userId'] == user_id:
+            if dct['completed'] == True:
+                done.append(dct['title'])
+                fin += 1
+            total += 1
+    print("Employee {} is done with tasks({}/{}):".format(employee, fin, total))
+    for i in done:
+        print("\t " + i)
