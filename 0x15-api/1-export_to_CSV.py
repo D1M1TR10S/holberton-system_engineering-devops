8e7ed8a32d8426a4eed5cdfc401add3a69126c83
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-02 16:30:15 +0000

    Changed employee name to username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,11 +13,10 @@ if __name__ == "__main__":
     if argv[1].isdigit():
         employee = requests.get(
                 'https://jsonplaceholder.typicode.com/users/{}'
-                .format(argv[1])).json().get('name')
+                .format(argv[1])).json().get('username')
         todo_list = requests.get(
                 'https://jsonplaceholder.typicode.com/todos?userId={}'
                 .format(argv[1])).json()
-    
         with open('{}.csv'.format(argv[1]), 'w') as todos:
             fieldnames = ["userId", "username", "completed", "title"]
             writer = csv.DictWriter(todos,
