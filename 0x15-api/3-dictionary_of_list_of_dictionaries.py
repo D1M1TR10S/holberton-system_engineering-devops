e255ea01a122734049af16346284cf5b257e882a
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-02 17:42:42 +0000

    Export data of all employee todo lists into JSON file

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+Retrieves information about all
+employees and their To-do lists
+through a RESTful API and then
+Exports this data into a JSON file
+'''
+if __name__ == "__main__":
+    import collections
+    import json
+    import requests
+    from sys import argv
+    
+    all_employees = requests.get(
+            'https://jsonplaceholder.typicode.com/users'
+            ).json()
+    dictionary = {}
+    for employee in all_employees:
+        todo_list = requests.get(
+                'https://jsonplaceholder.typicode.com/todos?userId={}'
+                .format(employee['id'])).json()
+
+        user_id = employee['id']
+        lst = []
+        dct = collections.OrderedDict()
+        for obj in todo_list:
+            dct["username"] = employee["username"]
+            dct["task"] = obj["title"]
+            dct["completed"] = obj["completed"]
+            lst.append(dict(dct))
+        dictionary[user_id] = lst
+    with open('todo_all_employees.json', 'w') as todos:
+        json.dump(dictionary, todos)
